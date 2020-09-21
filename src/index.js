0af09a304e1b823d2cae149019577c5cbae1638b
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-09-21 21:20:43 +0200

    added day and time in js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+let now = new Date();
+let h2 = document.querySelector("h2");
+let date = now.getDay();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+let day = days[now.getDay()];
+
+h2.innerHTML = `${day} ${hours}:${minutes}`;
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "10844e995ee350a31ff9d6258cf6674b";
