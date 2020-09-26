09c2e6d5f43aa810d227f4a4dabf54057c315798
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-09-26 18:46:41 +0200

    Current location search

--- src/index.js
+++ src/index.js
@@ -45,16 +45,28 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function search(city) {
+function searchCity(city) {
   let apiKey = "10844e995ee350a31ff9d6258cf6674b";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
+function searchLocation(position) {
+  let apiKey = "10844e995ee350a31ff9d6258cf6674b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
 function Mainsearch(event) {
   event.preventDefault();
   let theCityElement = document.querySelector("#the-city");
-  search(theCityElement.value);
+  searchCity(theCityElement.value);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -85,4 +97,7 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Amsterdam");
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+searchCity("Amsterdam");
