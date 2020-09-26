dfbdd6e85414a829fdf502b5ba47a987996a3eaf
Alinadot <71038633+Alinadot@users.noreply.github.com>
2020-09-26 22:13:39 +0200

    added time per day js

--- src/index.js
+++ src/index.js
@@ -22,6 +22,19 @@ let day = days[now.getDay()];
 
 h2.innerHTML = `${day} ${hours}:${minutes}`;
 
+function dayHours(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -54,7 +67,9 @@ function displayForecast(response) {
                 <span class="right-details">
                  <span class="first">
                     Sun
-                    <small class="day-time">12:00</small>
+                    <small class="day-time">${dayHours(
+                      forecast.dt * 1000
+                    )}</small>
                   <img src= "http://openweathermap.org/img/wn/${
                     forecast.weather[0].icon
                   }@2x.png"
